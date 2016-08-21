14d7379999ea87e08a48cbe2633936c786cbefce
Mansour Javaher <javaher.mansour@gmail.com>
2016-08-22 11:11:51 +1200

    Quit POP UP DIALOG

--- src/view/BoardFrame.java
+++ src/view/BoardFrame.java
@@ -3,10 +3,12 @@ package view;
 import cluedo.Cluedo;
 import cluedo.Player;
 
+
 import javax.swing.*;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -80,7 +82,23 @@ public class BoardFrame extends JFrame {
 
         // Quit
         cbMenuItem = new JCheckBoxMenuItem("Quit");
-        cbMenuItem.addActionListener(e -> System.exit(0));
+
+
+
+        cbMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				int action = JOptionPane.showConfirmDialog(BoardFrame.this,
+						"Do you really want to exit the application?",
+						"Confirm Exit", JOptionPane.OK_CANCEL_OPTION);
+
+				if (action == JOptionPane.OK_OPTION) {
+					System.exit(0);
+				}
+			}
+		});
+
+
         menu.add(cbMenuItem);
 
 
